0724ef5cdd8e0e3bc4eb86e6566121bbfed44272
mesaykk <mesay.kabew@hotmail.com>
2019-07-25 21:55:12 -0700

    Intial Commit

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
-const express = require('express')
-const app = express(),
+const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+//Db config
+const db = require('./config/keys').mongoURI;
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDb connected'))
+  .catch(err => console.log(err));
+
+
+app.get('/', (req, res) => res.send('Hello world'));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+
+const port = 5004;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
 
